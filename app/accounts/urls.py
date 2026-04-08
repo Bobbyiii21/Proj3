9c4663ecc97cc0f3ee1557df63362287db6ef0b5
Xavier <xevans7@gatech.edu>
2026-04-08 03:36:40 -0400

    Homepage frontend update

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('login/', views.login, name='accounts.login'),
     path('logout/', views.logout, name='accounts.logout'),
     #path('onboard/', views.onboard, name='accounts.onboard'),
-    #path('<str:user_link>/', views.profiles, name='accounts.profiles'),
+    path('<str:user_link>/', views.profiles, name='accounts.profiles'),
 ]
--- app/home/views.py
+++ app/home/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
 
 
 def index(request):
+    if request.user.is_authenticated:
+        user_name = request.user.first_name or request.user.username
+        return render(request, 'home/welcome.html', {
+            'user_name': user_name,
+            'template_data': {'title': 'Welcome back'}
+        })
+
     template_data = {}
     template_data['title'] = 'Chef++'
     return render(request, 'home/index.html', {'template_data': template_data})
